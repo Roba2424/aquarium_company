bf282ee99725482b5cf057e88eb36927299b134c
Robert Davidyan <robert.davidyan@Roberts-MacBook-Pro.local>
2024-06-17 01:17:31 +0400

    feat:added filling out the contact form for sending and checking

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ blueFish.addEventListener("click", (e) => {
   blueFish.style.marginTop = getRandomNum() + "px";
   blueFish.style.transition = "all 2s";
 });
+
+document.querySelector(".contact-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  if (name && email && subject) {
+    alert("Thank you for contacting us!");
+  } else {
+    alert("Please fill in all required fields.");
+  }
+});
