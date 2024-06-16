bc409bbf4ff82d1435794eb2be0af52d85f0bd48
Robert Davidyan <robert.davidyan@Roberts-MacBook-Pro.local>
2024-06-16 22:09:13 +0400

    feat:added feature to change position of the fish.style:general style change

---
+++ script.js
@@ -0,0 +1,13 @@
+const blueFish = document.querySelector(".blue-fish");
+
+function getRandomNum() {
+ return  Math.floor(Math.random() * 500) + 1;
+}
+
+blueFish.addEventListener("click", (e) => {
+  blueFish.style.marginLeft = getRandomNum() + "px";
+  blueFish.style.marginTop = getRandomNum() + "px";
+  blueFish.style.transition = 'all 2s'
+  blueFish.style.
+  console.log(blueFish.style.left);
+});
