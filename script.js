0b38d679a421bdc507abd2a37ff75b34ef286196
Robert Davidyan <robert.davidyan@Roberts-MacBook-Pro.local>
2024-06-16 22:19:56 +0400

    checking

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const blueFish = document.querySelector(".blue-fish");
 
 function getRandomNum() {
- return  Math.floor(Math.random() * 500) + 1;
+  return Math.floor(Math.random() * 500) + 1;
 }
 
 blueFish.addEventListener("click", (e) => {
   blueFish.style.marginLeft = getRandomNum() + "px";
   blueFish.style.marginTop = getRandomNum() + "px";
-  blueFish.style.transition = 'all 2s'
-  blueFish.style.
-  console.log(blueFish.style.left);
+  blueFish.style.transition = "all 2s";
 });
